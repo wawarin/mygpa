a63509c4470903d6f94553252fd255706c695769
Nuttakan Chuntra <nut.ch40@gmail.com>
2019-01-16 04:13:51 +0700

    Edit incorect variables

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import csv
 
-with open('mygpa.csv') as fp:
-  fr = csv.reader(fp, delimiter=',')
+with open('mygpa.csv') as csv_file:
+  csv_reader = csv.reader(csv_file, delimiter=',')
   line_count = 0
   for row in csv_reader:
     if line_count == 0:
