5cf2dab12697b27f2a69cb03ccc80c807366fd5f
Nuttakan Chuntra <nut.ch40@gmail.com>
2019-01-16 21:11:33 +0700

    Reading CSV file into a dictionary

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import csv
 
 with open('mygpa.csv') as csv_file:
-  csv_reader = csv.reader(csv_file, delimiter=',')
+  csv_reader = csv.DictReader(csv_file, delimiter=',')
   line_count = 0
   for row in csv_reader:
     if line_count == 0:
       print(f'Column names are {", ".join(row)}')
       line_count += 1
     else:
-      print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
+      print(f'\t{row["id"]} studies in the {row["course_name"]} course, and got {row["grade"]}.')
       line_count += 1
   print(f'Processed {line_count} lines.')
