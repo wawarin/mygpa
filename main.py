232e10e0771c0b28e5fe9247aeb432ac5703f55d
Nuttakan Chuntra <nut.ch40@gmail.com>
2019-01-16 21:13:47 +0700

    Edit incorrect column name (id => user_id)

--- main.py
+++ main.py
@@ -8,6 +8,6 @@ with open('mygpa.csv') as csv_file:
       print(f'Column names are {", ".join(row)}')
       line_count += 1
     else:
-      print(f'\t{row["id"]} studies in the {row["course_name"]} course, and got {row["grade"]}.')
+      print(f'\t{row["user_id"]} studies in the {row["course_name"]} course, and got {row["grade"]}.')
       line_count += 1
   print(f'Processed {line_count} lines.')
